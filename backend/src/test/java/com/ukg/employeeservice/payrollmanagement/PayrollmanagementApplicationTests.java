01e7f5eb200afb8d833aab8d69bb595719d82038
Bhuvnesh Bhatt <bhuvnesh.bhatt@ukg.com>
2024-08-12 14:10:20 +0530

    first commit

---
+++ backend/src/test/java/com/ukg/employeeservice/payrollmanagement/PayrollmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukg.employeeservice.payrollmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
